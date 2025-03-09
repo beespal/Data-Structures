3292773591d34228bc16bc0ce26e4dc0a657510d
beespal <bespalov.ao@physics.msu.ru>
2025-03-09 20:45:45 +0300

    added traverse functions

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <deque>
 template <typename T>
 class Node
 {
@@ -62,5 +64,37 @@ public:
             root = new_node;
         }
         add_node(value, root);
+    }
+    void show_node_depth(Node<T> *ptr)
+    {
+        if (ptr == nullptr)
+        {
+            return;
+        }
+        show_node_depth(ptr->left);
+        std::cout << ptr->data << ' ';
+        show_node_depth(ptr->right);
+    }
+    void show_tree_depth()
+    {
+        show_node_depth(root);
+    }
+    void show_tree_width()
+    {
+        std::deque<Node<T> *> nodes;
+        nodes.push_front(root);
+        while (!nodes.empty())
+        {
+            if (nodes.front()->left != nullptr)
+            {
+                nodes.push_back(nodes.front()->left);
+            }
+            if (nodes.front()->right != nullptr)
+            {
+                nodes.push_back(nodes.front()->right);
+            }
+            std::cout << nodes.front()->data << ' ';
+            nodes.pop_front();
+        }
     }
 };
